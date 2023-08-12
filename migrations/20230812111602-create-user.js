79fac1ce24521d93cfb7cbafcda06831d66a250f
vincentkho67 <vincentkho67@gmail.com>
2023-08-12 18:21:07 +0700

    model generate

---
+++ migrations/20230812111602-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN
+      },
+      is_student: {
+        type: Sequelize.BOOLEAN
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/datadetail.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DataDetail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DataDetail.init({
+    ojt_data_id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'DataDetail',
+  });
+  return DataDetail;
+};
