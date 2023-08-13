5c47521b44ffe2370ec6a4b60b566a44f07e17fb
vincentkho67 <vincentkho67@gmail.com>
2023-08-13 18:58:34 +0700

    auth users endpoint

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const AuthorizationMiddleware = require('../middlewares/auth');
 const UserController = require('../controllers/user');
 
-router.get('/', UserController.getAll);
-router.get('/:id', UserController.getOne);
-router.post('/', UserController.create);
-router.post('/login', UserController.login);
-router.post('register', UserController.register);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.get('/', AuthorizationMiddleware.authAdmin, UserController.getAll);
+router.get('/:id', AuthorizationMiddleware.authAdmin, UserController.getOne);
+router.post('/', AuthorizationMiddleware.authAdmin, UserController.create);
+router.post('/login', AuthorizationMiddleware.authAdmin, UserController.login);
+router.post('register', AuthorizationMiddleware.authAdmin, UserController.register);
+router.put('/:id', AuthorizationMiddleware.authAdmin, UserController.update);
+router.delete('/:id', AuthorizationMiddleware.authAdmin, UserController.delete);
 
 module.exports = router;
