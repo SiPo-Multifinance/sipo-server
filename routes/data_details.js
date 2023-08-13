ba8104afa29157cebfb0dbd729f12782f588c00b
vincentkho67 <vincentkho67@gmail.com>
2023-08-13 20:48:58 +0700

    hotfix bentar

--- routes/data_details.js
+++ routes/data_details.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const AuthorizationMiddleware = require('../middlewares/auth');
 const DataDetailsController = require('../controllers/data_details');
 
-router.get('/', AuthorizationMiddleware.authAdmin, AuthorizationMiddleware.authStudent, AuthorizationMiddleware.authViewer, DataDetailsController.getAll);
-router.get('/:id', AuthorizationMiddleware.authAdmin, AuthorizationMiddleware.authStudent, AuthorizationMiddleware.authViewer, DataDetailsController.getOne);
+router.get('/', DataDetailsController.getAll);
+router.get('/:id', DataDetailsController.getOne);
 router.post('/', AuthorizationMiddleware.authAdmin, AuthorizationMiddleware.authStudent, DataDetailsController.create);
 router.put('/:id', AuthorizationMiddleware.authAdmin, DataDetailsController.update);
 router.delete('/:id', AuthorizationMiddleware.authAdmin, DataDetailsController.delete);
--- routes/odp_groups.js
+++ routes/odp_groups.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const AuthorizationMiddleware = require('../middlewares/auth');
 const ODPGroupController = require('../controllers/odp_group');
 
-router.get('/', AuthorizationMiddleware.authAdmin, AuthorizationMiddleware.authStudent, AuthorizationMiddleware.authViewer, ODPGroupController.getAll);
-router.get('/:id', AuthorizationMiddleware.authAdmin, AuthorizationMiddleware.authStudent, AuthorizationMiddleware.authViewer, ODPGroupController.getOne);
+router.get('/', ODPGroupController.getAll);
+router.get('/:id', ODPGroupController.getOne);
 router.post('/', AuthorizationMiddleware.authAdmin, ODPGroupController.create);
 router.put('/:id', AuthorizationMiddleware.authAdmin, ODPGroupController.update);
 router.delete('/:id', AuthorizationMiddleware.authAdmin, ODPGroupController.delete);
