376e15a0100eff8b86aaff485d5d3b1bdd19274b
vincentkho67 <vincentkho67@gmail.com>
2023-08-13 19:31:34 +0700

    fix error

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const UserController = require('../controllers/user');
 router.get('/', AuthorizationMiddleware.authAdmin, UserController.getAll);
 router.get('/:id', AuthorizationMiddleware.authAdmin, UserController.getOne);
 router.post('/', AuthorizationMiddleware.authAdmin, UserController.create);
-router.post('/login', AuthorizationMiddleware.authAdmin, UserController.login);
+router.post('/login', UserController.login);
 router.post('register', AuthorizationMiddleware.authAdmin, UserController.register);
 router.put('/:id', AuthorizationMiddleware.authAdmin, UserController.update);
 router.delete('/:id', AuthorizationMiddleware.authAdmin, UserController.delete);
