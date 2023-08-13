f07e94dc7bc8e182be1412d307d6a6838d0513eb
vincentkho67 <vincentkho67@gmail.com>
2023-08-13 10:50:22 +0700

    mantap

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ const config = {
         password: process.env.MYSQLPASSWORD,
         database: process.env.MYSQLDATABASE,
         host: process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT,
         dialect: 'mysql'
     }
 }
