796e4292e86222eb428992a45afa5494957888b3
vincentkho67 <vincentkho67@gmail.com>
2023-08-16 06:20:58 +0700

    auth on nano_details

--- routes/nano_details.js
+++ routes/nano_details.js
@@ -5,8 +5,8 @@ const NanoDetailsController = require('../controllers/nano_details');
 
 router.get('/', NanoDetailsController.getAll);
 router.get('/:id', NanoDetailsController.getOne);
-router.post('/', NanoDetailsController.create);
-router.put('/:id', NanoDetailsController.update);
-router.delete('/:id', NanoDetailsController.delete);
+router.post('/',  AuthorizationMiddleware.authStudent, AuthorizationMiddleware.authAdmin,NanoDetailsController.create);
+router.put('/:id', AuthorizationMiddleware.authAdmin,NanoDetailsController.update);
+router.delete('/:id', AuthorizationMiddleware.authAdmin, NanoDetailsController.delete);
 
 module.exports = router;
