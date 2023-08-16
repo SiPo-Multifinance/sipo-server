07e69119f3453551783768506e90548f3f6ae766
vincentkho67 <vincentkho67@gmail.com>
2023-08-16 07:03:50 +0700

    add params

--- routes/odp_groups.js
+++ routes/odp_groups.js
@@ -6,7 +6,7 @@ const ODPGroupController = require('../controllers/odp_group');
 router.get('/', AuthorizationMiddleware.authViewer, ODPGroupController.getAll);
 router.get('/:id', AuthorizationMiddleware.authViewer, ODPGroupController.getOne);
 router.post('/', AuthorizationMiddleware.authAdmin,ODPGroupController.create);
-router.post('/assign', AuthorizationMiddleware.authAdmin,ODPGroupController.assignStudents);
+router.post('/assign/:id', AuthorizationMiddleware.authAdmin,ODPGroupController.assignStudents);
 router.put('/:id', AuthorizationMiddleware.authAdmin, ODPGroupController.update);
 router.delete('/:id', AuthorizationMiddleware.authAdmin, ODPGroupController.delete);
 
